3ef8b2bbd52ed710968b468fe9c02cd1027bb949
Woongjo Yoo <onikss793@gmail.com>
2020-03-09 15:38:54 +0900

    Part3 Quest 11 퀘스트 코드 작성 완료

---
+++ Part 3. node.js/Quest 11. My little web server/checklist.md
@@ -0,0 +1,14 @@
+## Topics
+
+-   GET/POST
+-   node.js `http` module
+
+    -   `req`와 `res` 객체
+
+## Checklist
+
+-   HTTP의 GET과 POST 메소드는 어떻게 다른가요?
+    -   다른 HTTP 메소드에는 무엇이 있나요?
+-   HTTP 서버에 GET과 POST를 통해 데이터를 보내려면 어떻게 해야 하나요?
+    -   HTTP 요청의 `Content-Type` 헤더는 무엇인가요?
+    -   Postman에서 POST 요청을 보내는 여러 가지 방법(`form-data`, `x-www-form-urlencoded`, `raw`, `binary`) 각각은 어떤 용도를 가지고 있나요?
--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,18 @@
-const http = require('http');
+const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
 
 http.createServer((req, res) => {
-	// TODO: 이 곳을 채워넣으세요..!
+    // TODO: 이 곳을 채워넣으세요..!
+    const URL = url.parse(req.url);
+    const QUERY = querystring.parse(URL.query);
+
+    if (URL.pathname === "/foo") {
+        const text = QUERY.bar;
+
+        (req.method === "GET" || req.method === "POST") &&
+            res.end(`Hello, ${text}`);
+    } else {
+        res.end("Hello World!");
+    }
 }).listen(8080);
