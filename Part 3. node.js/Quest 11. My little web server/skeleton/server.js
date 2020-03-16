190f9af4a67dc3335ea3f2c610414c19ecaffa23
Woongjo Yoo <onikss793@gmail.com>
2020-03-16 12:18:16 +0900

    Part 3. Quest 10 V8 엔진 내용 추가 및 Quest 11 server.js 수정

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -4,15 +4,117 @@ const querystring = require("querystring");
 
 http.createServer((req, res) => {
     // TODO: 이 곳을 채워넣으세요..!
-    const parsedUrl = url.parse(req.url);
+    const parsed_url = url.parse(req.url);
 
-    if (parsedUrl.pathname === "/foo") {
-        const parsedQuery = querystring.parse(parsedUrl.query);
-        const text = parsedQuery.bar;
+    route(parsed_url, req, res);
+}).listen(8080, () => console.log("Listening to PORT: ", 8080));
 
-        (req.method === "GET" || req.method === "POST") &&
-            res.end(`Hello, ${text}`);
-    } else {
-        res.end("Hello World!");
+function route(url, req, res) {
+    if (url.pathname === "/foo") {
+        switch (req.method) {
+            case "GET":
+                text = handleGetFoo(req, res, url);
+
+            case "POST":
+                text = handlePostFoo(req, res, url);
+        }
     }
-}).listen(8080);
+}
+
+function handlePostFoo(req, res) {
+    req.on("data", data => {
+        const body = encodeBody(req, data);
+
+        res.end(responseForBar(body));
+    });
+}
+
+function encodeBody(req, data) {
+    const contentType = detectMultipartFormData(req)
+        ? "multipart/form-data"
+        : req.headers["content-type"];
+
+    switch (contentType) {
+        case "application/json": {
+            const body = JSON.parse(data);
+
+            return body;
+        }
+
+        case "application/x-www-form-urlencoded": {
+            const body = urlEncoded(data);
+
+            return body;
+        }
+
+        case "multipart/form-data": {
+            const body = data.toString();
+
+            const arr = body
+                .split("----------------------------")
+                .join("")
+                .split("\r\n");
+
+            const encodedBody = getKeyValue(arr);
+
+            return encodedBody;
+        }
+    }
+}
+
+function getKeyValue(arr) {
+    let key = "";
+    let value = "";
+    let objects = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+
+        if (el === "" && i < arr.length - 1) {
+            value += arr[i + 1];
+        }
+
+        if (el.match("Content-Disposition: form-data; ")) {
+            key += el.slice(el.indexOf('"') + 1, el.lastIndexOf('"'));
+        }
+
+        if (key.length && value.length) {
+            objects.push({ [key]: value });
+
+            key = "";
+            value = "";
+        }
+    }
+
+    return objects.reduce((acc, curr) => {
+        return { ...curr, ...acc };
+    });
+}
+
+function detectMultipartFormData(req) {
+    return req.headers["content-type"].match("multipart/form-data");
+}
+
+function handleGetFoo(req, res, url) {
+    const query = querystring.parse(url.query);
+
+    res.end(responseForBar(query.bar));
+}
+
+function responseForBar(body) {
+    const text = body && body.bar;
+
+    return `Hello, ${text}`;
+}
+
+function urlEncoded(buffer) {
+    const stringifiedBuffer = buffer.toString();
+
+    const body = stringifiedBuffer
+        .split("&")
+        .map(str => str.split("="))
+        .map(pair => pair.reduce((acc, curr) => ({ [acc]: curr })))
+        .reduce((acc, curr) => ({ ...acc, ...curr }));
+
+    return body;
+}
